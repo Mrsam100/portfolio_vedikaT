1a8083b1cc4b48811745dd9240456086588c53c3
Mrsam100 <sukhjeetkhalsa111@gmail.com>
2025-07-11 14:12:36 +0530

    localhost change

--- index.ts
+++ index.ts
@@ -62,15 +62,16 @@ const server = createServer(app);
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-const port = 5000;
+const port = process.env.PORT || 5000;
+
 server.listen(
   {
     port,
-    host: "127.0.0.1", // ✅ only the host here
+    host: "0.0.0.0", // ✅ This allows external access
     reusePort: true,
   },
   () => {
-    log(`✅ Serving on http://127.0.0.1:${port}`);
+    log(`✅ Serving on http://0.0.0.0:${port}`);
   }
 );
 })();
