f8800b4c7ddf6501f511db22a806cb32aa19adb9
Mrsam100 <sukhjeetkhalsa111@gmail.com>
2025-07-11 14:47:13 +0530

    production mode

--- server/index.ts
+++ server/index.ts
@@ -53,11 +53,12 @@ const server = createServer(app);
   // importantly only setup vite in development and after
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
-  if (app.get("env") === "development") {
-    await setupVite(app, server);
-  } else {
-    serveStatic(app);
-  }
+ if (process.env.NODE_ENV === "production") {
+  serveStatic(app);  
+} else {
+  await setupVite(app, server);  
+}
+
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client.
